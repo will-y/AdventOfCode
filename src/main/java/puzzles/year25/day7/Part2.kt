38c78f21122b768ea245ea411073a255cfa088ba
Will Yelton <willhyelton@gmail.com>
2025-12-07 01:06:16 -0500

    Small performace increase

--- src/main/java/puzzles/year25/day7/Part2.kt
+++ src/main/java/puzzles/year25/day7/Part2.kt
@@ -4,9 +4,9 @@ import util.Puzzle
 
 class Part2 : Puzzle<Long?> {
     override fun getAnswer(inputString: String): Long {
-        val lines = inputString.trim().lines()
+        val lines = inputString.trim().lines().map { it.toCharArray() }
         val waysToGetHere: Array<Array<Long>> = Array(lines.size + 1) {
-            Array(lines[0].length) { 0 };
+            Array(lines[0].size) { 0 }
         }
 
         waysToGetHere[0][lines[0].indexOf('S')] = 1
@@ -15,6 +15,7 @@ class Part2 : Puzzle<Long?> {
             for (j in lines[i].indices) {
                 val c = lines[i][j]
                 val waysToGetC = waysToGetHere[i][j]
+                if (waysToGetC == 0L) continue
                 if (c == '^') {
                     waysToGetHere[i + 1][j - 1] += waysToGetC
                     waysToGetHere[i + 1][j + 1] += waysToGetC
